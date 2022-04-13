5fffa21231c6e48f8482d5012fb220f5b557ecc7
Gabriel Andrade <gabrielhbandrade@hotmail.com>
2022-04-13 15:44:59 +0900

    Adjust scoring values

--- util/evaluation.py
+++ util/evaluation.py
@@ -15,95 +15,108 @@ def score(gold, predicted):
     matched = set()
     results = dict.fromkeys(
         ["exact_match", "exceeding_match", "exceeding_match_overlap", "partial_match", "partial_match_overlap",
-         "sub_match", "missing_match", "incorrect_match"], 0)
+         "missing_match", "incorrect_match"], 0)
 
-    for match in predicted:
-        found_gold_match = False
-        pred_start, pred_end = match['span']
-        for i in range(len(gold)):
-            gold_start, gold_end = gold[i]['span']
+    for entity in gold:
+        found_match = False
+        gold_start, gold_end = entity['span']
+        gold_tag = entity['type']
+        for i in range(len(predicted)):
+            pred_start, pred_end = predicted[i]['span']
+            pred_tag = predicted[i]['type']
 
-            # Skip gold tags that came before
-            if pred_start >= gold_end:
+            if found_match:
+                break
+
+            # Skip different tags
+            if gold_tag != pred_tag:
+                continue
+
+            # Skip prediction tags that came before
+            if gold_start >= pred_end:
                 continue
 
             # We can stop searching now
-            elif pred_end <= gold_start:
+            elif gold_end <= pred_start:
                 break
 
             # Exact match
-            elif match['span'] == gold[i]['span']:
-                score += 3
-                results["exact_match"] += 1
-                matched.add(gold[i]['span'])
-                found_gold_match = True
+            elif pred_start == gold_start and pred_end == gold_end:
+                if not found_match:
+                    score += 1
+                    results["exact_match"] += 1
+                    matched.add(predicted[i]['span'])
+                    found_match = True
 
             # Exceeding match, but not overlap next entity
             elif pred_start <= gold_start and pred_end >= gold_end:
+
+                # Check for overlap
                 if i + 1 < len(gold):
-                    if pred_end < gold[i + 1]['span'][0]:  # next gold start
-                        score += 2
-                        results["exceeding_match"] += 1
-                        matched.add(gold[i]['span'])
-                        found_gold_match = True
-                    else:
-                        score -= 1
-                        results["exceeding_match_overlap"] += 1
-                        matched.add(gold[i]['span'])
-                        found_gold_match = True
-                else:
-                    score += 2
+                    if not pred_end < gold[i + 1]['span'][0]:  # next gold start
+                        if not found_match:
+                            score += 0
+                            results["exceeding_match_overlap"] += 1
+                            matched.add(predicted[i]['span'])
+                            found_match = True
+                            continue
+
+                if not found_match:
+                    score += 0.5
                     results["exceeding_match"] += 1
-                    matched.add(gold[i]['span'])
-                    found_gold_match = True
+                    matched.add(predicted[i]['span'])
+                    found_match = True
 
             # Partial match
+            elif pred_start >= gold_start and pred_end <= gold_end:
+                if not found_match:
+                    score += 0.5
+                    results["partial_match"] += 1
+                    matched.add(predicted[i]['span'])
+                    found_match = True
+
             elif (pred_start >= gold_start and pred_end >= gold_end) or (
                     pred_start <= gold_start and pred_end <= gold_end):
-                if i + 1 < len(gold):
-                    if pred_end < gold[i + 1]['span'][0]:  # next gold start
-                        score += 0
-                        results["partial_match"] += 1
-                        matched.add(gold[i]['span'])
-                        found_gold_match = True
-                    else:
-                        score -= 1
-                        results["partial_match_overlap"] += 1
-                        matched.add(gold[i]['span'])
-                        found_gold_match = True
-                else:
-                    score += 0
-                    results["partial_match"] += 1
-                    matched.add(gold[i]['span'])
-                    found_gold_match = True
 
-            # Sub-match
-            elif pred_start >= gold_start and pred_end <= gold_end:
-                score += 0
-                results["sub_match"] += 1
-                matched.add(gold[i]['span'])
-                found_gold_match = True
+                # Check for overlap
+                if i + 1 < len(gold):
+                    if not pred_end < gold[i + 1]['span'][0]:  # next gold start
+                        if not found_match:
+                            score += 0
+                            results["partial_match_overlap"] += 1
+                            matched.add(predicted[i]['span'])
+                            found_match = True
+                            continue
+
+                if not found_match:
+                    score += 0.5
+                    results["partial_match"] += 1
+                    matched.add(predicted[i]['span'])
+                    found_match = True
 
             else:
                 raise Exception("Insanity-check, should not be here!")
 
-        if not found_gold_match:
-            score -= 1
-            results["incorrect_match"] += 1
+        if not found_match:
+            score += 0
+            results["missing_match"] += 1
 
-    missing_match = len(gold) - len(matched)
-    score -= 3 * missing_match
-    results["missing_match"] = missing_match
+    incorrect_match = len(predicted) - len(matched)
+    score += 0
+    results["incorrect_match"] = incorrect_match
     pprint(results)
     if len(gold):
-        return score / (len(gold) * 3)
-    return score
+        return score / (len(gold))
+    return float(score)
 
 
 if __name__ == '__main__':
     O = 'O'
     B = 'B'
     I = 'I'
+
+    m = '-m'
+    d = '-d'
 
     gold = [O, O, B, I, I, I, O, O, O, O, B, I, I, I, I, O, O]
     test = [O, O, B, I, I, I, O, O, O, O, B, I, I, I, I, O, O]
@@ -170,6 +183,27 @@ if __name__ == '__main__':
 
     #      [O, O, B, I, I, I, O, O, O, O, B, I, I, I, I, O, O]
     test = [O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O]
+    print(gold)
+    print(test)
+    print(score(gold, test))
+    print('\n')
+
+    gold = [O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O]
+    test = [O, O, B, I, I, I, O, O, O, O, B, I, I, I, I, O, O]
+    print(gold)
+    print(test)
+    print(score(gold, test))
+    print('\n')
+
+    gold = [O, O, B, I, I, I, O, O, O, O, B, I, I, I, O, O, O]
+    test = [O, B, I, O, O, B, I, O, O, B, I, O, O, B, I, O, O]
+    print(gold)
+    print(test)
+    print(score(gold, test))
+    print('\n')
+
+    gold = [O, O, B + m, I + m, I + m, I + m, O, O, O, O, B + d, I + d, I + d, I + d, I + d, O, O]
+    test = [O, O, B + m, I + m, I + m, I + m, O, O, O, O, B + m, I + m, I + m, I + m, I + m, O, O]
     print(gold)
     print(test)
     print(score(gold, test))
