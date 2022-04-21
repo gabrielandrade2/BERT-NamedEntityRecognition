4dbc8292588086245e5f406fffc90df6facb9fc5
Gabriel Andrade <gabrielhbandrade@hotmail.com>
2022-04-21 19:10:59 +0900

    Yakureki symptom tag statistics + normalization script

--- knowledge_bases/meddra.py
+++ knowledge_bases/meddra.py
@@ -51,13 +51,24 @@ class MedDRADatabase:
         cursor.execute(
             'SELECT llt_code, llt_name, pt_code FROM llt'
         )
-        return pd.DataFrame(cursor.fetchall(), columns=['llt_code', 'llt_name', 'pt_code'])
+        return pd.DataFrame(cursor.fetchall(), columns=['llt_code', 'llt_name'])
+
+    def get_all_pt(self):
+        """ Get a Dataframe representing the table containing all English PTs.
+
+        :return: A Dataframe containing all terms' pt_code and pt_name.
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'SELECT pt_code, pt_name FROM pt'
+        )
+        return pd.DataFrame(cursor.fetchall(), columns=['pt_code', 'pt_name'])
 
     def get_llt_j(self, llt_code):
         """ Get the Japanese Low-level term (LLT) name from its code.
 
         :param llt_code: The code of the term.
-        :return: The Japanese term term or None, if a match is not found.
+        :return: The Japanese term or None, if a match is not found.
         """
         cursor = self.conn.cursor()
         cursor.execute(
@@ -65,16 +76,51 @@ class MedDRADatabase:
         )
         return cursor.fetchone()[0]
 
-    def get_all_llt_j(self):
-        """ Get a Dataframe representing the table containing all Japanese LLTs.
+    def get_llt_j_code(self, llt_kanji):
+        """ Get the Japanese Low-level term (LLT) code from its kanji.
 
-        :return: A Dataframe containing all Japanese terms' llt_code, llt_kanji, referring pt_code and llt_kana.
+        :param llt_kanji: The kanji of the term.
+        :return: The term code or None, if a match is not found.
         """
         cursor = self.conn.cursor()
         cursor.execute(
-            'SELECT llt_code, llt_name, pt_code, llt_kana FROM llt_j'
+            'SELECT llt_code FROM llt_j WHERE llt_kanji =  {}'.format(llt_kanji)
         )
-        return pd.DataFrame(cursor.fetchall(), columns=['llt_code', 'llt_kanji', 'pt_code', 'llt_kana'])
+        return cursor.fetchone()[0]
+
+    def get_all_llt_j(self):
+        """ Get a Dataframe representing the table containing all Japanese LLTs.
+
+        :return: A Dataframe containing all Japanese terms' llt_code, llt_kanji and llt_kana.
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'SELECT llt_code, llt_kanji, llt_kana FROM llt_j'
+        )
+        return pd.DataFrame(cursor.fetchall(), columns=['llt_code', 'llt_kanji', 'llt_kana'])
+
+    def get_pt_j_code(self, pt_kanji):
+        """ Get the Japanese Preferred term (PT) code from its kanji.
+
+        :param pt_kanji: The kanji of the term.
+        :return: The term code or None, if a match is not found.
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'SELECT pt_code FROM pt_j WHERE pt_kanji =  {}'.format(pt_kanji)
+        )
+        return cursor.fetchone()[0]
+
+    def get_all_pt_j(self):
+        """ Get a Dataframe representing the table containing all Japanese PTs.
+
+        :return: A Dataframe containing all Japanese terms' pt_code, pt_kanji and pt_kana.
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'SELECT pt_code, pt_kanji, pt_kana FROM pt_j'
+        )
+        return pd.DataFrame(cursor.fetchall(), columns=['pt_code', 'pt_kanji', 'pt_kana'])
 
     def get_all_en_jp_llt(self):
         cursor = self.conn.cursor()
@@ -92,14 +138,14 @@ class MedDRADatabase:
         """
         cursor = self.conn.cursor()
         cursor.execute(
-            'SELECT pt_kanji FROM llt INNER JOIN pt_j ON llt.pt_code = pt_j.pt_code WHERE llt_code = {}'.format(
+            'SELECT pt_kanji, pt_j.pt_code FROM llt INNER JOIN pt_j ON llt.pt_code = pt_j.pt_code WHERE llt_code = {}'.format(
                 llt_code))
         res = cursor.fetchall()
 
         if not res:
             return None
         else:
-            return res[0][0]
+            return res[0][0], res[0][1]
 
 
 class MedDRAPatientFriendlyList:
@@ -114,9 +160,46 @@ class MedDRAPatientFriendlyList:
         return list(self.df[['LLT kanji', 'LLT code', 'Level']].itertuples(index=False, name=None))
 
 
+class MedDRAEntityNormalizer(EntityNormalizer):
+
+    def __init__(self, database: MedDRADatabase, matching_method=fuzz.token_set_ratio, threshold=0):
+        self.database = database
+        self.matching_method = matching_method
+        self.threshold = threshold
+
+        self.llt_list = database.get_all_llt_j()
+
+    def __match_llt(self, term):
+        temp = list()
+        for index, llt in self.llt_list.iterrows():
+            score = self.matching_method(term, llt['llt_kanji'])
+            temp.append((score, llt))
+        preferred_candidate = max(temp, key=lambda i: i[0])
+
+        score = preferred_candidate[0]
+        if score > self.threshold:
+            term = preferred_candidate[1]['llt_kanji']
+            llt_code = preferred_candidate[1]['llt_code']
+            return term, llt_code
+        return None
+
+    def normalize(self, term):
+        ret = self.__match_llt(term)
+        if not ret:
+            return term
+        return self.database.get_pt_j_from_llt_code(ret[1])
+
+    def normalize_to_llt(self, term):
+        ret = self.__match_llt(term)
+        if not ret:
+            return term
+        return ret[0]
+
+
 class MedDRAPatientFriendlyPTEntityNormalizer(EntityNormalizer):
 
-    def __init__(self, database, patient_friendly_list, matching_method=fuzz.token_set_ratio, threshold=0):
+    def __init__(self, database: MedDRADatabase, patient_friendly_list, matching_method=fuzz.token_set_ratio,
+                 threshold=0):
         self.database = database
         self.patient_friendly_list = patient_friendly_list
         self.pf_list = patient_friendly_list.get_patient_friendly_tuples()
@@ -132,7 +215,7 @@ class MedDRAPatientFriendlyPTEntityNormalizer(EntityNormalizer):
             if patient_friendly_term[2] == 'pt':
                 normalized_term = patient_friendly_term[0]
             else:
-                normalized_term = self.database.get_pt_j_from_llt_code(patient_friendly_term[1])
+                normalized_term, _ = self.database.get_pt_j_from_llt_code(patient_friendly_term[1])
         else:
             normalized_term = term
 
--- util/text_utils.py
+++ util/text_utils.py
@@ -83,6 +83,42 @@ def remove_tags(text, tag_list=None):
     return text
 
 
+def findstem(arr):
+    arr = sorted(arr, key=lambda x: len(x))
+
+    # Determine size of the array
+    n = len(arr)
+
+    # Take first word from array
+    # as reference
+    s = arr[0]
+    l = len(s)
+
+    res = ""
+
+    for i in range(l):
+        for j in range(i + 1, l + 1):
+
+            # generating all possible substrings
+            # of our reference string arr[0] i.e s
+            stem = s[i:j]
+            k = 1
+            for k in range(1, n):
+
+                # Check if the generated stem is
+                # common to all words
+                if stem not in arr[k]:
+                    break
+
+            # If current substring is present in
+            # all strings and its length is greater
+            # than current result
+            if (k + 1 == n and len(res) < len(stem)):
+                res = stem
+
+    return res
+
+
 class EntityNormalizer(ABC):
 
     @abstractmethod
